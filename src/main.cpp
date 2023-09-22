fd7ecfd1d4134c0981c0305b944f58e66d6486a1
BallzCrasher <shadisalman11@gmail.com>
2023-09-22 19:03:16 +0300

    change input method from stdin to argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,47 @@
 #include "qoi_image.hpp"
+#include "argparse.hpp"
 
 #include <algorithm>
 #include <cctype>
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <exception>
+#include <format>
 
-std::string getFilePath()
+constexpr std::string_view help_message =
+"Usage: {} [-o OUTFILE][-r COUNT][-i][-f] <INFILE>";
+
+
+int32_t main(int32_t argc, const char* argv[])
 {
-	std::string filePath{};
-	while (true)
-	{
-		std::cout << "Specify image file path: ";
-		std::getline(std::cin, filePath);
-
-		if (!std::filesystem::exists(filePath))
-		{
-			std::cerr << "Invalid image file path!\n";
-			continue;
-		}
-		return filePath;
+	try {
+		QOI::Config args{argc, argv};
+	} catch (std::exception& exception) {
+		std::cerr << std::format(help_message, argv[0]) << '\n';
+		std::cerr << "Error: " <<  exception.what() << '\n';
+		exit(EXIT_FAILURE);
 	}
-}
+	QOI::Config args{argc, argv};
+	QOI::Image	imageFile{std::string{args.inFilePath}};
+	
 
-void getResponse(std::string_view message, std::string& response)
-{
-	while (true)
-	{
-		std::cout << message;
-		std::getline(std::cin, response);
-		size_t size{response.size()};
-		if (size != 1)
-		{
-			std::cerr << "Invalid response! Type y for Yes and n for No.\n";
-			continue;
-		}
-		response[0] = tolower(response[0]);
-		break;
-	}
-}
-
-void getResponse(std::string_view message, int32_t& response)
-{
-	while (true)
-	{
-		std::cout << message;
-		std::string input{};
-		std::getline(std::cin, input);
-		if (!std::all_of(input.begin(), input.end(), [&](char c) { return isdigit(c); }) ||
-			input.front() == '0')
-		{
-			std::cerr << "Invalid response! Please enter a positive integer.\n";
-
-			continue;
-		}
-		response = std::stoi(input);
-		break;
-	}
-}
-
-int32_t main()
-{
-	std::string response{};
-	std::string filePath{getFilePath()};
-	QOI::Image	imageFile{filePath};
-
-	getResponse("Do you want to flip the image? (y/n) ", response);
-	if (response == "y")
+	if (args.flip)
 	{
 		imageFile.flipImage();
 	}
 
-	getResponse("Do you want to invert the image? (y/n) ", response);
-	if (response == "y")
+	if (args.invert)
 	{
 		imageFile.invertImage();
 	}
 
-	getResponse("Do you want to rotate the image to the left? (y/n) ", response);
-	if (response == "y")
+	if (args.rotations > 0)
 	{
-		int32_t times{};
-		getResponse("How many times? ", times);
-		imageFile.rotateImageLeft(times);
+		imageFile.rotateImageLeft(args.rotations);
 	}
-	imageFile.writeImage("outputImage");
-	std::cout << "Modified image has been written into \"outputImage.qoi\".\n";
-	return 0;
+
+	imageFile.writeImage(std::string{args.outFilePath});
+	std::cout << "Modified image has been written into \"" 
+		<< args.outFilePath<< "\".\n";
+	return 0; 
 }
